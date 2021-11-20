2e642f3349b3820c22560babc93c05b02fe22868
LAPTOP-OPMNB0HF\Lenovo <golubrysa@gmail.com>
2021-11-20 20:19:22 +0500

    add tasks 21

---
+++ tasks/task_21/homework21.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+name = input("Введите название жанра: ")
+
+connection = sqlite3.connect("music_db_1.sqlite")
+cursor = connection.cursor()
+
+res = cursor.execute("""SELECT Album.ArtistId, Album.title FROM Album
+    WHERE AlbumId IN (SELECT Track.AlbumId FROM Track
+        WHERE GenreId=(SELECT GenreId FROM Genre WHERE Name='Metal' ))""").fetchall()
+res = list(map(lambda x: x[-1], sorted(res)))
+
+print(*res, sep="\n")
