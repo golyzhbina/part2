3cb79595b15fd56740f51de3a0ee33b544048452
LAPTOP-OPMNB0HF\Lenovo <golubrysa@gmail.com>
2021-12-04 11:46:17 +0500

    make edits

--- tasks/task_21/homework21.py
+++ tasks/task_21/homework21.py
@@ -5,9 +5,9 @@ name = input("Введите название жанра: ")
 connection = sqlite3.connect("music_db_1.sqlite")
 cursor = connection.cursor()
 
-res = cursor.execute("""SELECT Album.ArtistId, Album.title FROM Album
+res = cursor.execute(f"""SELECT Album.ArtistId, Album.title FROM Album
     WHERE AlbumId IN (SELECT Track.AlbumId FROM Track
-        WHERE GenreId=(SELECT GenreId FROM Genre WHERE Name='Metal' ))""").fetchall()
+        WHERE GenreId=(SELECT GenreId FROM Genre WHERE Name='{name}' ))""").fetchall()
 res = list(map(lambda x: x[-1], sorted(res)))
 
 print(*res, sep="\n")
