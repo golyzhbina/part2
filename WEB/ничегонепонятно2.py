cca35dfaa323e5e263ff8465e1cc0359a5833b0a
LAPTOP-OPMNB0HF\Lenovo <golubrysa@gmail.com>
2022-02-08 14:18:21 +0500

    pain

---
+++ WEB/ничегонепонятно2.py
@@ -0,0 +1,27 @@
+import os
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from flask import Flask, render_template, redirect
+from dotenv import load_dotenv
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+load_dotenv()
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv('MY_SECRET_KEY')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
